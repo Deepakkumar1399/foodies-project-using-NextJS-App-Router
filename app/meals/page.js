49f819dbb4ad368992560fea44414525e42c9d17
Deepak kumar <kumarsahudeepak1999@gmail.com>
2024-06-17 23:03:01 +0530

    adding db and fetch meals

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,19 @@
 import Link from 'next/link';
+import { Suspense } from 'react';
+
 import classes from './page.module.css';
 import MealsGrid from '@/components/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
+
+
+async function Meals(){
+    const meals = await getMeals();
+    
+    return <MealsGrid meals={meals}/>
+}
+
 export default function MealsPage(){
+    
     return(
         <>
         <header className={classes.header}>
@@ -16,7 +28,9 @@ export default function MealsPage(){
             </p>
         </header>
         <main className={classes.main}>
-            <MealsGrid meals={[]}/>
+            <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+                <Meals/>
+            </Suspense>
         </main>
         </>    
     )
