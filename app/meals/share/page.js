95e9f0f6cc80b48c15e620e3f73b8ff165c3f3a3
Deepak kumar <kumarsahudeepak1999@gmail.com>
2024-06-20 21:18:17 +0530

    Added the code for meal action and inputValidation

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,7 +1,12 @@
+'use client';
+import {useFormState} from 'react-dom';
 import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
+import { shareMeal } from '@/lib/actions';
+import MealsFormSubmit from '@/components/meals/meals-form-submit';
 
 export default function ShareMealPage() {
+  const [state, formAction]= useFormState(shareMeal, {message:null});
   return (
     <>
       <header className={classes.header}>
@@ -11,7 +16,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -39,9 +44,10 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePicker/>
+          <ImagePicker label="Your image" name="image"/>
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit/>
           </p>
         </form>
       </main>
